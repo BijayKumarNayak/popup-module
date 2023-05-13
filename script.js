eb176ad2c952be55f2ab0d5dbd40a9db4b74a9c1
BijayKumarNayak <91744852+BijayKumarNayak@users.noreply.github.com>
2023-05-13 20:33:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function show(){
+    document.getElementById("module").style.display="block";
+}
+function hide(){
+    document.getElementById("module").style.display="none";
+
+}
+function handlesubmit(){
+ alert("Thanks for intrest")
+    
+   
+}
